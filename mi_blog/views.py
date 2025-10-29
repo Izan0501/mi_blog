06f40c688e173e3d920dca567c600a351a96e06b
izan <ivozanacchi501@gmail.com>
2025-10-29 15:16:03 -0300

    addedUserAuth--login-register-logout

--- mi_blog/views.py
+++ mi_blog/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
 
 # render home page
 def home(request):
     return render(request, 'pages/home.html')
+
+# register user
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # inicia sesión automáticamente
+            return redirect('login')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'auth/register.html', {'form': form})
+
+# login user
+def login_view(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomAuthenticationForm()
+    return render(request, 'auth/login.html', {'form': form})
+
+# logout user
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
